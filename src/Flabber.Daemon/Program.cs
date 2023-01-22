dd9d1e9c420429fbc07513f8b3d9728b4e2fc846
Ch. König <me@kurabloodlust.eu>
2023-01-22 12:10:31 +0100

    initial commit

---
+++ src/Flabber.Daemon/Program.cs
@@ -0,0 +1,6 @@
+﻿// Program.cs Copyright (c) z0ne.
+// All Rights Reserved.
+// Licensed under the EUPL 1.2 License.
+// See LICENSE the project root for license information.
+
+Console.WriteLine("Hello, World!");
